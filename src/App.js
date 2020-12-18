c844ecd600a549858a778a0f12f20d41ce0a062d
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-12-17 19:45:45 -0500

    app displays all responses from keyword banana

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Recipe from './Recipe';
+require('dotenv').config()
+const MY_APP_ID = process.env.REACT_APP_APP_ID;
+const MY_APP_KEY = process.env.REACT_APP_APP_KEY;
 
-function App() {
+const App = () => {
+  const API_URL = `https://api.edamam.com/search?q=banana&app_id=${MY_APP_ID}&app_key=${MY_APP_KEY}`;
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  });
+
+
+ const loadData = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='App'>
+        <h1>Calling Edamam Recipe API with search query "Banana"</h1>
+     {
+        recipes.map((r,id) => (
+          <Recipe key={id} title={r.recipe.title} image={r.recipe.image} calories={r.recipe.calories} />
+        ))
+      }
     </div>
-  );
-}
+    </>
+  )
+};
 
 export default App;
